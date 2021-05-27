4a2b5e0f621482efc6e99194bd48a9181d7b7528
penny-clark <respect.cactus@gmail.com>
2021-05-26 20:04:21 -0400

    Complete and test initial project set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {!data ? "Loading..." : data}
         </p>
         <a
           className="App-link"
